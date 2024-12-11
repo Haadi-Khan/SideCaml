bfd5b3121a027a11a8ff4d35840fa390e89ef82c
Will Bradley <wjb247@cornell.edu>
2024-12-10 22:28:20 -0500

    update test file with printing and equality-test abstraction
    - also added transpose test

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -2,18 +2,30 @@ open OUnit2
 open Final_project.Transformer
 open Final_project.Matrix
 
+let m1 = of_array [| [| 1.; 2. |]; [| 3.; 4. |] |]
+let m2 = of_array [| [| 5.; 6. |]; [| 7.; 8. |] |]
 
-let test_matrix_dot _ =
-  let m1 = of_array [| [| 1.; 2. |]; [| 3.; 4. |] |] in
-  let m2 = of_array [| [| 5.; 6. |]; [| 7.; 8. |] |] in
-  let expected = of_array [| [| 19.; 22. |]; [| 43.; 50. |] |] in
-  assert_equal expected (dot m1 m2)
+let print m =
+  m 
+  |> to_array 
+  |> Array.map Array.to_list 
+  |> Array.to_list 
+  |> List.map (fun row -> "\n" ^ String.concat ", " @@ List.map Float.to_string row)
+  |> String.concat ""
 
-let test_matrix_dot_single _ =
+  let eq_test expected_arr x =
+  fun _ -> assert_equal (of_array expected_arr) x ~printer:print
+
+let test_matrix_dot =
+  eq_test [| [| 19.; 22. |]; [| 43.; 50. |] |] (dot m1 m2)
+
+let test_matrix_dot_single =
   let m1 = of_array [| [| 2. |] |] in
   let m2 = of_array [| [| 3. |] |] in
-  let expected = of_array [| [| 6. |] |] in
-  assert_equal expected (dot m1 m2)
+  eq_test [| [| 6. |] |] (dot m1 m2)
+
+let test_matrix_transpose =
+  eq_test [| [| 1.; 3. |]; [| 2.; 4.; |] |] (transpose m1)
 
 let () =
   run_test_tt_main
@@ -21,4 +33,5 @@ let () =
     >::: [
            "test_matrix_dot" >:: test_matrix_dot;
            "test_matrix_dot_single" >:: test_matrix_dot_single;
+           "test_matrix_transpose" >:: test_matrix_transpose
          ])
