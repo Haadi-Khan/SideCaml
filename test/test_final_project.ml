c98e075ae46e1f9a7da42cc2c1e4c7e5ef9f5a70
Will Bradley <wjb247@cornell.edu>
2024-12-10 21:45:07 -0500

    fix type errors in the test file

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -1,25 +1,24 @@
-(* open OUnit2
+open OUnit2
 open Final_project.Transformer
+open Final_project.Matrix
 
-let test_hello _ = assert_equal 2 (1 + 1)
 
 let test_matrix_dot _ =
-  let m1 = [| [| 1.; 2. |]; [| 3.; 4. |] |] in
-  let m2 = [| [| 5.; 6. |]; [| 7.; 8. |] |] in
-  let expected = [| [| 19.; 22. |]; [| 43.; 50. |] |] in
-  assert_equal expected (Matrix.dot m1 m2)
+  let m1 = of_array [| [| 1.; 2. |]; [| 3.; 4. |] |] in
+  let m2 = of_array [| [| 5.; 6. |]; [| 7.; 8. |] |] in
+  let expected = of_array [| [| 19.; 22. |]; [| 43.; 50. |] |] in
+  assert_equal expected (dot m1 m2)
 
 let test_matrix_dot_single _ =
-  let m1 = [| [| 2. |] |] in
-  let m2 = [| [| 3. |] |] in
-  let expected = [| [| 6. |] |] in
-  assert_equal expected (Matrix.dot m1 m2)
+  let m1 = of_array [| [| 2. |] |] in
+  let m2 = of_array [| [| 3. |] |] in
+  let expected = of_array [| [| 6. |] |] in
+  assert_equal expected (dot m1 m2)
 
 let () =
   run_test_tt_main
     ("test suite"
     >::: [
-           "test_hello" >:: test_hello;
            "test_matrix_dot" >:: test_matrix_dot;
            "test_matrix_dot_single" >:: test_matrix_dot_single;
-         ]) *)
+         ])
