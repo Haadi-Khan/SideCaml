1276da117466ec73d32ae5379fdbeea1d4b94465
Jiming Chen <jc3579@cornell.edu>
2024-12-10 19:15:13 -0500

    Format comments

--- lib/transformer.mli
+++ lib/transformer.mli
@@ -5,7 +5,8 @@ val load_posts : string -> post list
 (** [load_posts filename] loads a list of posts from a JSON file. *)
 
 val prepare_training_data : post list -> string
-(** [prepare_training_data posts] extracts the text from a list of posts and concatenates them into a single string. *)
+(** [prepare_training_data posts] extracts the text from a list of posts and
+    concatenates them into a single string. *)
 
 val scaled_dot_product_attention :
   float array array ->
@@ -13,34 +14,36 @@ val scaled_dot_product_attention :
   Matrix.t ->
   Matrix.t option ->
   Matrix.t
-(** [scaled_dot_product_attention query key value mask] computes the scaled dot product attention. *)
+(** [scaled_dot_product_attention query key value mask] computes the scaled dot
+    product attention. *)
 
 val multi_head_attention :
-  t ->
-  Matrix.t ->
-  Matrix.t ->
-  Matrix.t ->
-  Matrix.t option ->
-  Matrix.t
-(** [multi_head_attention config query key value mask] computes the multi-head attention. *)
+  t -> Matrix.t -> Matrix.t -> Matrix.t -> Matrix.t option -> Matrix.t
+(** [multi_head_attention config query key value mask] computes the multi-head
+    attention. *)
 
 val transformer_block : t -> Matrix.t -> Matrix.t
-(** [transformer_block config input] applies a transformer block to the input matrix. *)
+(** [transformer_block config input] applies a transformer block to the input
+    matrix. *)
 
 val prepare_input : 'a array -> Matrix.t
 (** [prepare_input input] converts an array of arrays into a matrix. *)
 
 val sample_from_distribution : float array -> int
-(** [sample_from_distribution distribution] samples an index from a categorical distribution. *)
+(** [sample_from_distribution distribution] samples an index from a categorical
+    distribution. *)
 
 val sample_with_temperature : float array -> float -> int
-(** [sample_with_temperature distribution temperature] samples an index from a categorical distribution with a temperature. *)
+(** [sample_with_temperature distribution temperature] samples an index from a
+    categorical distribution with a temperature. *)
 
 val beam_search : float array -> int -> int -> int
-(** [beam_search distribution beam_size max_length] performs beam search to generate a sequence of tokens. *)
+(** [beam_search distribution beam_size max_length] performs beam search to
+    generate a sequence of tokens. *)
 
 val is_repetitive : int array -> int -> bool
-(** [is_repetitive sequence n] checks if the last n tokens in the sequence are repetitive. *)
+(** [is_repetitive sequence n] checks if the last n tokens in the sequence are
+    repetitive. *)
 
 val forward_pass : t -> int array -> float array
 (** [forward_pass model input] computes the forward pass of the model. *)
@@ -61,4 +64,5 @@ val clean_text : string -> string
 (** [clean_text text] cleans the text by removing special characters. *)
 
 val position_encoding : int -> int -> Matrix.t
-(** [position_encoding max_length embedding_dim] computes the position encoding matrix. *)
+(** [position_encoding max_length embedding_dim] computes the position encoding
+    matrix. *)
